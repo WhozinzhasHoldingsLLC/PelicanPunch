7a80b9a8404c9f4ceddfbefdb20891f4cbe4216b
TeslaThomas <ironblackdog@yahoo.com>
2015-09-07 09:50:31 -0400

    Initial commit

---
+++ Pelican Punch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pelican Punch
+//
+//  Created by The Burkharts on 9/7/15.
+//  Copyright (c) 2015 Whozinzhas Holdings. LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pelican Punch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Pelican Punch
+//
+//  Created by The Burkharts on 9/7/15.
+//  Copyright (c) 2015 Whozinzhas Holdings. LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
